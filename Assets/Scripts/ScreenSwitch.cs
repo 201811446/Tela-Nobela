5fa081365d04a805f9f01ee0388c04dc5dada0b3
201811446 <84395838+201811446@users.noreply.github.com>
2021-09-30 18:17:52 +0800

    Sept 30

---
+++ Assets/Scripts/ScreenSwitch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenSwitch : MonoBehaviour
+{
+    
+    public void Start()
+    {
+        
+    }
+
+    public void DressUp()
+    {
+        
+    }
+}
