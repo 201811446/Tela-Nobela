5a8ad4c561b5495bb2886f2b764dc2af494103cd
201811446 <201811446@feualabang.edu.ph>
2021-11-07 15:41:45 +0800

    clear + screenshot + timer added

--- Assets/Scripts/ScreenShot.cs
+++ Assets/Scripts/ScreenShot.cs
@@ -19,13 +19,13 @@ public class ScreenShot : MonoBehaviour
     private IEnumerator Screenshot()
     {
         yield return new WaitForEndOfFrame();
-        Texture2D texture = new Texture2D(Screen.width / 0, Screen.height, TextureFormat.RGB24, false);
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
 
-        texture.ReadPixels(new Rect(Screen.width / 0, 0, Screen.width / 0, Screen.height), 0, 0);
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         texture.Apply();
 
         byte[] bytes = texture.EncodeToPNG();
-        File.WriteAllBytes("D:/Drive D/Desktop/Screenshot.png", bytes);
+        File.WriteAllBytes(Application.dataPath + "/Screenshot.png", bytes);
 
         Destroy(texture);
     }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Timer : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class Timer : MonoBehaviour
      // Start is called before the first frame update
      void Start()
      {
-         currentTime = startMinutes * 30;
+         currentTime = startMinutes * 60;
      }
 
      // Update is called once per frame
@@ -21,9 +22,15 @@ public class Timer : MonoBehaviour
      {
          if (timerActive == true)
          {
-             currentTime = currentTime = Time.deltaTime;
+             currentTime = currentTime - Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                timerActive = false;
+                Start();
+            }
          }
-         currentTimeText.text = currentTime.ToString();
+         TimeSpan time = TimeSpan.FromSeconds(currentTime);
+         currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
      }
 
      public void StartTime()
