c19c03801a6a2bbc572d1fb9306611e83b25a5d7
201811446 <201811446@feualabang.edu.ph>
2021-11-10 19:13:40 +0800

    sa

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CoinCounter : MonoBehaviour
+{
+
+    public int coins;
+    public Text currentcoins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentcoins.text = coins.ToString();
+    }
+
+    public void CoinInc()
+    {
+        coins += 1;
+    }
+
+}
+
+
+  
